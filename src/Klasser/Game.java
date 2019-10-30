389f7849f63074d66c4d8005db59ad1ab3bf25e2
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2019-10-30 10:25:26 +0100

    Game class created

--- src/Klasser/Game.java
+++ src/Klasser/Game.java
@@ -1,5 +1,17 @@
 package Klasser;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class Game {
+    private Brick[] bricks;
+    private Text step = new Text("0");
+    private Button reset = new Button("New Game");
+    private GridPane centerGrid = new GridPane();
+    private GridPane leftGrid = new GridPane();
+
+    private Brick[] getBricks() { return bricks; }
+    public Text getStep(){ return step; }
 
 }
