dcfaeda884a41935dffdae6a146b508cde70e9b7
Jaanek Kerma <Jaanek.Kerma@yh.nackademin.se>
2019-10-30 10:21:21 +0100

    Brick class created

--- src/Klasser/Brick.java
+++ src/Klasser/Brick.java
@@ -3,5 +3,22 @@ package Klasser;
 import javafx.scene.control.Button;
 
 public class Brick extends Button {
+    private int nummer;
+    private int row;
+    private int col;
 
+    int getNummer() { return nummer; }
+
+    int getRow() { return row; }
+    int getCol() { return col; }
+    void setPos(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    private Brick(int nummer){
+        this.nummer = nummer;
+        setText(Integer.toString(nummer));
+        setPrefSize(50, 50);
+    }
 }
